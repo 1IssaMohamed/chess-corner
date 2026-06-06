5f72bdcf454ab1461207855c00c729ed3f424baa
1IssaMohamed <issamoh@my.yorku.ca>
2026-06-06 04:30:41 -0400

    fixed actions adn readme too

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: process.env.GITHUB_ACTIONS ? "/chess-corner/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
